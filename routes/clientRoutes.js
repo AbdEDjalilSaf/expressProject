7874937855580135796ec4fa1df27ab27f14d446
Abdo saf <noodsaf01@gmail.com>
2025-08-12 12:21:12 +0100

    second  commit

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { protect } from '../middleware/authMiddleware';
+// import { protect } from '../middleware/authMiddleware';
 
 
 import * as clientController from '../controller/clientController'
